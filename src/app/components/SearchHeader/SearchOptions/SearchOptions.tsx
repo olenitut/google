9126ebac2aa03c4cf7e5847df3fb760c73ec735c
Ana Hordieieva <gordeevaisgreat@gmail.com>
2023-10-02 15:32:05 +0300

    add google api

--- src/app/components/SearchHeader/SearchOptions/SearchOptions.tsx
+++ src/app/components/SearchHeader/SearchOptions/SearchOptions.tsx
@@ -14,7 +14,7 @@ const SearchOptions = () => {
   return (
     <div className="flex w-full border-b-[1px] gap-5 text-sm">
       <Link
-        className={`flex gap-1 items-center pb-4 border-b-4 border-transparent ml-[18%] ${
+        className={`flex gap-1 items-center pb-3 border-b-4 border-transparent ml-[18%] ${
           activeLink === 1 && "text-blue-700 !border-blue-700"
         }`}
         href={`/search/web?query=${query}`}
@@ -23,7 +23,7 @@ const SearchOptions = () => {
         <p>All</p>
       </Link>
       <Link
-        className={`flex gap-1 items-center pb-4 border-b-4 border-transparent  ${
+        className={`flex gap-1 items-center pb-3 border-b-4 border-transparent  ${
           activeLink === 2 && "text-blue-700 !border-blue-700"
         }`}
         href={`/search/image?query=${query}`}
--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,7 +1,40 @@
-import styles from "./page.module.scss";
+interface ISearchItem {
+  kind: string;
+  title: string;
+  htmlTitle: string;
+  link: string;
+  displayLink: string;
+  snippet: string;
+  htmlSnippet: string;
+  formattedUrl: string;
+  htmlFormattedUrl: string;
+  pagemap: {
+    cse_thumbnail: [];
+    periodical: [];
+    metatags: [];
+    cse_image: [];
+    scholarlyarticle: [];
+    listitem: [];
+  };
+}
 
-const SearchPage = () => {
-  return <div>dsf</div>;
+const SearchPage = async ({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}) => {
+  const res =
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.query}
+  `);
+  const data = await res.json();
+
+  console.log(data.items);
+
+  return (
+    <div>
+      {data && data.items.map((el: ISearchItem) => <h1>{el.title}</h1>)}
+    </div>
+  );
 };
 
 export default SearchPage;
